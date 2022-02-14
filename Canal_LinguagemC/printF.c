b181f29aed28efdfc434ad4c7b9c9e2b46c26d40
PauloVieira <paulovieira@discente.ufg.br>
2022-02-14 08:35:08 -0300

    comando printf

---
+++ Canal_LinguagemC/printF.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    char letra = 'E';//caracteres
+    int nro = 5;//inteiros
+    printf("Letra %c, numero %d\n", letra, nro);//exibir na tela
+
+}
