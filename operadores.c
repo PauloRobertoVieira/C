d607e86968b9487d1df1448712e1faa7da9de853
PauloVieira <paulovieira@discente.ufg.br>
2022-02-22 23:02:12 -0300

    operadores

---
+++ operadores.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    /* operadores relacional*/
+    //> < >= <= == !=
+    int x = 5, y = 3, z = 8;
+
+    printf("5 > 3 ? se true então  %d\n", x>y);
+    printf("5 < 3 ? se false então %d\n", x<y);
+    printf("é igual %d então é true\n", x+y == z);
+}
